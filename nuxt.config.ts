54ec9ca7826a088fc860d23319e2fc0622a44384
Rwagsu <84891987+Rwagsu@users.noreply.github.com>
2025-11-19 16:17:27 +0800

    还是老网页端吧(悲)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,49 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  extends: ['shadcn-docs-nuxt'],
+    devtools: { enabled: true },
+    extends: ['shadcn-docs-nuxt'],
 
-  content: {
-      highlight: {
-          langs: ['csharp', 'xml', 'kotlin', 'groovy', 'kts', 'java', 'log', 'rust', 'properties', 'toml'],
-      },
+    content: {
+        highlight: {
+            langs: ['csharp', 'xml', 'kotlin', 'groovy', 'kts', 'java', 'log', 'rust', 'properties', 'toml'],
+        },
 
-    database: { type: 'memory' } 
-  },
+        preview: {
+            api: 'https://api.nuxt.studio',
+            gitInfo: {
+                name: 'RwagsuOwO',
+                owner: 'Rwagsu',
+                url: 'https://github.com/Rwagsu/RwagsuOwO'
+            }
+        }
+    },
 
-  studio: {
-      repository: {
-          provider: 'github',
-          owner: 'Rwagsu',
-          repo: 'RwagsuOwO',
-          branch: 'shadcn-docs',
-      }
-  },
+    fonts: {
+        providers: {
+            google: false,
+            googleicons: false,
+        },
+    },
 
-  fonts: {
-      providers: {
-          google: false,
-          googleicons: false,
-      },
-  },
+    i18n: {
+        defaultLocale: 'zh',
 
-  i18n: {
-      defaultLocale: 'zh',
+        locales: [
+            {
+                code: 'zh',
+                name: '中文 (简体)',
+                language: 'zh-CN',
+            },
+            {
+                code: 'en',
+                name: 'English',
+                language: 'en-US',
+            }
+        ],
+    },
 
-      locales: [
-          {
-              code: 'zh',
-              name: '中文 (简体)',
-              language: 'zh-CN',
-          },
-          {
-              code: 'en',
-              name: 'English',
-              language: 'en-US',
-          }
-      ],
-  },
-
-  compatibilityDate: '2025-11-09',
-  modules: ['@nuxt/content', 'nuxt-studio'],
+    compatibilityDate: '2025-11-09',
+    modules: [],
 });
