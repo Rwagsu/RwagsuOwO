26bf807512691406ec1860b819f68a8280584183
Rwagsu <84891987+Rwagsu@users.noreply.github.com>
2026-01-27 17:43:08 +0800

    这多语言怎么那么难弄啊

--- components/search.tsx
+++ components/search.tsx
@@ -1,75 +1,46 @@
-"use client";
-import { useSearch } from "basehub/react-search";
-import type { SortedResult } from "fumadocs-core/server";
+'use client';
 import {
-  SharedProps,
   SearchDialog,
-} from "fumadocs-ui/components/dialog/search";
-import { useMemo } from "react";
+  SearchDialogClose,
+  SearchDialogContent,
+  SearchDialogHeader,
+  SearchDialogIcon,
+  SearchDialogInput,
+  SearchDialogList,
+  SearchDialogOverlay,
+  type SharedProps,
+} from 'fumadocs-ui/components/dialog/search';
+import { useDocsSearch } from 'fumadocs-core/search/client';
+import { create } from '@orama/orama';
+import { useI18n } from 'fumadocs-ui/contexts/i18n';
 
-export function Search({
-  _searchKey,
-  ...props
-}: SharedProps & { _searchKey: string }) {
-  const search = useSearch({
-    _searchKey,
-    queryBy: ["_title", "richText", "category", "slug"],
+function initOrama() {
+  return create({
+    schema: { _: 'string' },
+    // https://docs.orama.com/docs/orama-js/supported-languages
+    language: 'english',
+  });
+}
+
+export default function DefaultSearchDialog(props: SharedProps) {
+  const { locale } = useI18n(); // (optional) for i18n
+  const { search, setSearch, query } = useDocsSearch({
+    type: 'static',
+    initOrama,
+    locale,
   });
 
-  const results = useMemo(() => {
-    if (!search.result || !search.result.found) return "empty";
-
-    return search.result.hits.flatMap((hit) => {
-      const items: SortedResult[] = [];
-      const url = hit.document.slug ? `/docs/${hit.document.slug}` : "/docs";
-
-      items.push({
-        id: hit._key,
-        content: (
-          <span className="font-medium">
-            {hit.highlight?._title ? (
-              <span
-                dangerouslySetInnerHTML={{
-                  __html: hit.highlight._title.snippet as string,
-                }}
-              />
-            ) : (
-              hit.document._title
-            )}
-          </span>
-        ) as unknown as string,
-        type: "page",
-        url,
-      });
-
-      for (const h of hit.highlights) {
-        if (!h.snippet || h.fieldPath === "title") continue;
-
-        items.push({
-          id: `${hit._key}-${h.fieldPath}`,
-          type: "text",
-          content: (
-            <span
-              dangerouslySetInnerHTML={{
-                __html: h.snippet as string,
-              }}
-            />
-          ) as unknown as string,
-          url,
-        });
-      }
-
-      return items;
-    });
-  }, [search.result]);
-
   return (
-    <SearchDialog
-      {...props}
-      onSearchChange={search.onQueryChange}
-      search={search.query}
-      isLoading={search.query.length > 0 && search.result === undefined}
-      results={results}
-    />
+    <SearchDialog search={search} onSearchChange={setSearch} isLoading={query.isLoading} {...props}>
+      <SearchDialogOverlay />
+      <SearchDialogContent>
+        <SearchDialogHeader>
+          <SearchDialogIcon />
+          <SearchDialogInput />
+          <SearchDialogClose />
+        </SearchDialogHeader>
+        <SearchDialogList items={query.data !== 'empty' ? query.data : null} />
+      </SearchDialogContent>
+    </SearchDialog>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
+import { createMDX } from 'fumadocs-mdx/next';
+
+const withMDX = createMDX();
+
 /** @type {import('next').NextConfig} */
 const config = {
+  output: 'export',
   reactStrictMode: true,
 };
 
-export default config;
+export default withMDX(config);
